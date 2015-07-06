cec453d6e7b98cc26330971b8518b8a40a9facd0
Mario Castro Squella <mario.castro.squella@gmail.com>
2015-07-06 16:42:53 -0400

    Setup UI

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -11,26 +11,17 @@ import org.scalajs.jquery.jQuery
  */
 object TutorialApp extends JSApp {
   def main(): Unit = {
-//    println("Hello world!")
+    jQuery(setupUI _)
+  }
+
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(addClickedMessage _)
     jQuery("body").append("<p>Hello world!</p>")
-    appendUl(document.body, List("First element", "Second Element", "Third Element"))
-  }
-
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
-  }
-
-  def appendUl(targetNode: dom.Node, list: List[String]): Unit = {
-    val ul = document.createElement("ul")
+    jQuery("body").append("<ul id=\"the-list\"></ul>")
+    val list = List("First element", "Second element")
     for (item <- list) {
-      val li = document.createElement("li")
-      li.appendChild(document.createTextNode(item))
-      ul.appendChild(li)
+      jQuery("#the-list").append("<li>" + item + "</li>")
     }
-    document.body.appendChild(ul)
   }
 
   @JSExport
