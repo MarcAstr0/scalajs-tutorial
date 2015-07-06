0d05cc8dcd7fbebefa5202811e309e21bdc40882
Mario Castro Squella <mario.castro.squella@gmail.com>
2015-07-06 16:04:53 -0400

    Added DOM elements

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 scalaJSStage in Global := FastOptStage
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,12 +1,33 @@
 package tutorial.webapp
 
 import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import dom.document
 
 /**
  * Created by mcastro on 06-07-2015.
  */
 object TutorialApp extends JSApp {
   def main(): Unit = {
-    println("Hello world!")
+//    println("Hello world!")
+    appendPar(document.body, "Hello world!")
+    appendUl(document.body, List("First element", "Second Element", "Third Element"))
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
+  def appendUl(targetNode: dom.Node, list: List[String]): Unit = {
+    val ul = document.createElement("ul")
+    for (item <- list) {
+      val li = document.createElement("li")
+      li.appendChild(document.createTextNode(item))
+      ul.appendChild(li)
+    }
+    document.body.appendChild(ul)
   }
 }
