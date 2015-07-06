6955aaa63c37f14e836619d105caceef3c41f5b1
Mario Castro Squella <mario.castro.squella@gmail.com>
2015-07-06 14:59:55 -0400

    First "Hello World"

---
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,12 @@
+package tutorial.webapp
+
+import scala.scalajs.js.JSApp
+
+/**
+ * Created by mcastro on 06-07-2015.
+ */
+object TutorialApp extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
